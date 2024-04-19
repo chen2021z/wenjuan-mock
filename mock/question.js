bf1923101562a8cb48f44744155162dbab3a9170
Chen <2934570984@qq.com>
2024-04-19 13:04:54 +0800

    feat: 模拟pageSize

--- mock/question.js
+++ mock/question.js
@@ -42,14 +42,15 @@ module.exports = [
     url: "/api/question",
     method: "get",
     response(ctx) {
-      const { url = "" } = ctx;
+      const { url = "", query = {} } = ctx;
       const isDeleted = url.includes("isDeleted=true");
       const isStar = url.includes("isStar=true");
+      const pageSize = parseInt(query.pageSize) || 10;
 
       return {
         errno: 0,
         data: {
-          list: getQuestionList({ isDeleted, isStar }), // 当前页数据
+          list: getQuestionList({ isDeleted, isStar, len: pageSize }), // 当前页数据
           total: 100,
         },
       };
