5bf701d5bc15de95e49460b541c4db33c08d1697
Chen <2934570984@qq.com>
2024-04-26 16:04:21 +0800

    feat: 定义问卷组件数据结构

--- mock/question.js
+++ mock/question.js
@@ -1,25 +1,30 @@
 const Mock = require("mockjs");
 const getQuestionList = require("./data/getQuestionList");
 const Random = Mock.Random;
+const getComponentList = require("./data/getComponentList");
 
 module.exports = [
   // 获取单个问卷
   {
+    // 获取单个问卷信息
     url: "/api/question/:id",
     method: "get",
-    // response() {
-    //   return {
-    //     errno: 100,
-    //     msg: 'this is a error'
-    //   };
-    // },
     response() {
       return {
         errno: 0,
         data: {
           id: Random.id(),
           title: Random.ctitle(),
+          desc: "问卷描述",
+          js: "",
+          css: "",
+          isDeleted: false,
+          isPublished: true,
+          componentList: getComponentList(),
         },
+
+        // errno: 1002,
+        // msg: '错误测试'
       };
     },
   },
