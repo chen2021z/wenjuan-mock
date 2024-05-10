e012a3453e0287e9c19d308b5d7ff0b9ed39cfa1
Chen <2934570984@qq.com>
2024-05-11 03:46:47 +0800

    feat: 新增答卷 api

--- mock/index.js
+++ mock/index.js
@@ -2,11 +2,13 @@ const test = require('./test')
 const question = require('./question')
 const user = require('./user')
 const stat = require('./stat')
+const answer = require('./answer')
 
 module.exports = [
   ...test,
   ...question,
   ...user,
-  ...stat
+  ...stat,
+  ...answer
 ]
 
